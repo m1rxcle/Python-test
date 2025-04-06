884c5fcfdb9d26fd266d51b2c8f0f40be2a30442
Denis M1rxcle <noonebesideu@gmail.com>
2025-04-06 14:21:23 +0200

    end second lesson

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ perimeter = round((first_side + second_side) * 2, 1)
 print(
     f"Площадь прямоугольника равна: {area}.\nПериметр прямоугольника равен: {perimeter}"
 )
+
+# Задание 2
+
+initian_number = 46275
+
+ten_thousands = initian_number // 10000
+thousands = initian_number // 1000 % 10
+hundreds = initian_number // 100 % 10
+tens = initian_number // 10 % 10
+ones = initian_number % 10
+
+result = (tens**ones) * hundreds / (ten_thousands - thousands)
+
+
+print(result)
