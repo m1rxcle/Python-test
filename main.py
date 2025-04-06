15005756b929bd9617672abc5bf1d7f1a78e7b3a
Denis M1rxcle <noonebesideu@gmail.com>
2025-04-06 18:27:37 +0200

    end fivth lesson

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ if find_palendrom == user_string:
     print("yes")
 else:
     print("no")
+
+# Задание №2
+
+new_user_string = input("Введите строку: ")
+
+if len(new_user_string) < 1000:
+    trim_string = " ".join(new_user_string.split())
+else:
+    print("Длина строки превышает 1000 символов")
+
+print(trim_string)
