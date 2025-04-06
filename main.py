58e2ed969bbb4f65b2f6c0dc061be4209e9047f6
Denis M1rxcle <noonebesideu@gmail.com>
2025-04-06 05:31:26 +0200

    first task

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Задание 1
+
+first_side = float(input("Введите длину первой стороны: "))
+second_side = float(input("Введите длину второй стороны: "))
+
+area = round(first_side * second_side, 1)
+perimeter = round((first_side + second_side) * 2, 1)
+
+print(
+    f"Площадь прямоугольника равна: {area}.\nПериметр прямоугольника равен: {perimeter}"
+)
