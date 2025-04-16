b93146df458a3edc472c101b06228367653de85f
Denis M1rxcle <noonebesideu@gmail.com>
2025-04-16 18:04:17 +0200

    end eleventh lesson

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-import random
-
 # Задание № 1
 
-n = int(input("Введите размер матрицы: "))
-item = int(input("Введите количество элементов в матрице: "))
-
-matrix_1 = []
-
-matrix_2 = []
-
-matrix_3 = []
-
-for i in range(n):
-    a = [random.randint(-100, 100) for _ in range(item)]
-    matrix_1.append(a)
-    b = [random.randint(-100, 100) for _ in range(item)]
-    matrix_2.append(b)
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 
-for i in range(n):
-    c = []
-    for j in range(item):
-        c.append(matrix_1[i][j] + matrix_2[i][j])
-    matrix_3.append(c)
+def recursive_print(l, index=0):
+    if index == len(l):
+        print("Конец списка")
+        return
+
+    print(l[index])
+    recursive_print(l, index + 1)
 
 
-print("Матрица №1: ", matrix_1)
-print()
-print("Матрица №2: ", matrix_2)
-print()
-print("Матрица №2: ", matrix_3)
+recursive_print(my_list)
